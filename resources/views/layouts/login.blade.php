86f0d8232513b5208fe9b7221634e0404bae045d
Saturngod <saturngod@gmail.com>
2020-10-26 14:02:52 +0630

    fixed login

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -34,15 +34,22 @@
                 <img src="{{ asset("static/logo.svg") }}" height="36" alt="">
             @show
         </div>
-        <form class="card card-md" action="." method="post">
-            @csrf
+      <form class="card card-md" action="{{route('auth.login')}}" method="post">
+        @csrf
           <div class="card-body">
             <h2 class="mb-5 text-center">Login to your account</h2>
             <div class="mb-3">
+             
               <label class="form-label">Email address</label>
-              <input type="email" class="form-control" placeholder="Enter email" autocomplete="off">
+              <input required type="email" name="email" class="form-control" placeholder="Enter email" autocomplete="off">
+
+              @error('email')
+                <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+
             </div>
             <div class="mb-2">
+              
               <label class="form-label">
                 Password
                 <span class="form-label-description">
@@ -50,16 +57,20 @@
                 </span>
               </label>
               <div class="input-group input-group-flat">
-                <input type="password" class="form-control"  placeholder="Password" >
+                <input required name="password" type="password" class="form-control"  placeholder="Password" >
                 <span class="input-group-text">
-                  <a href="#" class="link-secondary" title="Show password" data-toggle="tooltip"><svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><circle cx="12" cy="12" r="2" /><path d="M2 12l1.5 2a11 11 0 0 0 17 0l1.5 -2" /><path d="M2 12l1.5 -2a11 11 0 0 1 17 0l1.5 2" /></svg>
+                  <a href="#" class="link-secondary
+                  " title="Show password" data-toggle="tooltip"><svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><circle cx="12" cy="12" r="2" /><path d="M2 12l1.5 2a11 11 0 0 0 17 0l1.5 -2" /><path d="M2 12l1.5 -2a11 11 0 0 1 17 0l1.5 2" /></svg>
                   </a>
                 </span>
+                @error('password')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
               </div>
             </div>
             <div class="mb-2">
               <label class="form-check">
-                <input type="checkbox" class="form-check-input"/>
+                <input name="remember" type="checkbox" class="form-check-input"/>
                 <span class="form-check-label">Remember me on this device</span>
               </label>
             </div>
@@ -88,8 +99,8 @@
         <div class="text-center text-muted">
           Don't have account yet? <a href="./sign-up.html" tabindex="-1">Sign up</a>
         </div>
-        @show
       </div>
+      @show
     </div>
     <!-- Libs JS -->
     <script src="{{ asset("libs/bootstrap/dist/js/bootstrap.bundle.min.js")}}"></script>
